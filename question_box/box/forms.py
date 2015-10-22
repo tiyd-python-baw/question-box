021cf030975e785a027dfeb72357670f263180bd
Bob Amand <biotmg@nc.rr.com>
2015-10-22 17:22:47 -0400

    new question form

--- question_box/box/forms.py
+++ question_box/box/forms.py
@@ -1,2 +1,10 @@
 from django import forms
 from .models import Question
+
+
+class NewQuestion(forms.Form):
+    '''Form for adding a new question'''
+
+    class Meta:
+        model = Question
+        fields = ('title', 'text')
