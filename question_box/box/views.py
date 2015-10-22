a069075cddee5de56421040196bfc8be1a5c5325
Bob Amand <biotmg@nc.rr.com>
2015-10-22 16:23:13 -0400

    home page view

--- question_box/box/views.py
+++ question_box/box/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from .models import Question, Answers, Score
+from django.views.generic.list import ListView
+
 
 # Create your views here.
+
+class AllQuestionsView(ListView):
+    '''Used to list all questions on the Home page'''
+    template_name = 'questionhome.html'
+    context_object_name = 'questions'
+    paginate_by = 20
+
+    model = Question
+
+    def get_queryset(self):
+        preload = Question.objects.all()
+        return preload.order_by('-timestamp')
