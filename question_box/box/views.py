d58f85514c6132ba2e83b15faf2b8aef587c051c
William Butts <wbutts1023@gmail.com>
2015-10-22 15:40:53 -0400

    added a builtin login/logout, made registration but do not have the template for it yet.

--- question_box/box/views.py
+++ question_box/box/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
+
+
+def register_user(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            score = Score(user=user)
+            score.save()
+            user = authenticate(username=user.username,
+                                password=request.POST['password1'])
+            login(request, user)
+            #return redirect('home_page', request.user.username)
+
+    else:
+        form = UserCreationForm()
+    return render(request, 'box/register.html', {'form': form})
