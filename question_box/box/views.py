2aaef1e210420f3164be24f07c8741bb6f2e3491
apierce1992 <apierce1992@gmail.com>
2015-10-22 16:39:22 -0400

    stuff

--- question_box/box/views.py
+++ question_box/box/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
+from .models import Question, Answers, Score
+from django.contrib.auth.models import User
+from django.shortcuts import get_object_or_404
+
+
 
 # Create your views here.
-def question_detail(request, question_id):
-    question = Question.objects.get(id=question_id)
+def question_detail(request, question_pk):
+    #question = Question.objects.get(pk=question_pk)
+    question = get_object_or_404(Question, pk=question_pk)
     answers = Answers.objects.filter(question=question).all()
-    return render (request, 'question_detail.html', {'question':question,
+    return render (request, 'box/templates/question_detail.html', {'question':question,
                                                     'answers': answers})
 
 
