a5901c32d2bd5154d99732bf0900f58d0b10e459
William Butts <wbutts1023@gmail.com>
2015-10-24 13:25:26 -0400

    changed staticfiles settings.

--- question_box/question_box/heroku_settings.py
+++ question_box/question_box/heroku_settings.py
@@ -22,3 +22,7 @@ STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'question_box/static'),
+)
