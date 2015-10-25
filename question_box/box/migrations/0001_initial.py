22d6b9f420d5b7b6ae632d905023dba637a59196
William Butts <wbutts1023@gmail.com>
2015-10-25 19:17:33 -0400

    putting migrations on heroku

--- question_box/box/migrations/0001_initial.py
+++ question_box/box/migrations/0001_initial.py
@@ -16,30 +16,45 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Answers',
             fields=[
-                ('id', models.AutoField(serialize=False, primary_key=True, verbose_name='ID', auto_created=True)),
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('text', models.TextField(max_length=1000)),
-                ('timestamp', models.DateTimeField(default=datetime.datetime(2015, 10, 22, 20, 31, 37, 710659))),
+                ('timestamp', models.DateTimeField(default=datetime.datetime(2015, 10, 25, 22, 44, 35, 257502))),
                 ('points_a', models.IntegerField(default=0)),
             ],
         ),
         migrations.CreateModel(
             name='Question',
             fields=[
-                ('id', models.AutoField(serialize=False, primary_key=True, verbose_name='ID', auto_created=True)),
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('title', models.CharField(max_length=255)),
-                ('text', models.TextField(blank=True, null=True, max_length=1000)),
-                ('timestamp', models.DateTimeField(default=datetime.datetime(2015, 10, 22, 20, 31, 37, 709714))),
-                ('points_q', models.IntegerField(default=0)),
-                ('user', models.ForeignKey(to=settings.AUTH_USER_MODEL)),
+                ('text', models.TextField(null=True, max_length=1000, blank=True)),
+                ('timestamp', models.DateTimeField(default=datetime.datetime(2015, 10, 25, 22, 44, 35, 256356))),
             ],
         ),
         migrations.CreateModel(
             name='Score',
             fields=[
-                ('id', models.AutoField(serialize=False, primary_key=True, verbose_name='ID', auto_created=True)),
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('points', models.IntegerField(default=0)),
                 ('user', models.OneToOneField(to=settings.AUTH_USER_MODEL)),
             ],
+        ),
+        migrations.CreateModel(
+            name='Tag',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=255)),
+            ],
+        ),
+        migrations.AddField(
+            model_name='question',
+            name='tags',
+            field=models.ManyToManyField(related_name='questions', to='box.Tag'),
+        ),
+        migrations.AddField(
+            model_name='question',
+            name='user',
+            field=models.ForeignKey(to=settings.AUTH_USER_MODEL),
         ),
         migrations.AddField(
             model_name='answers',
@@ -50,5 +65,10 @@ class Migration(migrations.Migration):
             model_name='answers',
             name='user',
             field=models.ForeignKey(to=settings.AUTH_USER_MODEL),
+        ),
+        migrations.AddField(
+            model_name='answers',
+            name='voter',
+            field=models.ForeignKey(null=True, to=settings.AUTH_USER_MODEL, related_name='voter'),
         ),
     ]
